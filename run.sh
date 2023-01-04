9e5b3aef9eeca1c9c9d458a975d50e0a23f11ca0
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2023-01-05 00:23:20 +0900

    tweak

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 set -Ceu
 
+CURRENT_DIR=$(cd "$(dirname "${0}")" || exit 1; pwd)
+
 xhost +
 
 docker container run --rm \
 --platform='linux/i386' \
 -e 'DISPLAY=host.docker.internal:0' \
--v "$(pwd)/_rom:/_rom" \
+-v "${CURRENT_DIR}/_rom:/_rom" \
 --name pose \
 pose:latest &
